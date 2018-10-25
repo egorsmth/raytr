d63b9613622d273d37071bc31604194778fcc39e
egorsmth <mattspring0@gmail.com>
2018-10-25 10:41:11 +0300

    week 2

--- src/Main.cpp
+++ src/Main.cpp
@@ -36,7 +36,7 @@ hitable *random_scene() {
             vec3 center(a+0.9*drand48(), 0.2,b+0.9*drand48());
             if ((center-vec3(4,0.2,0)).length() > 0.9) {
                 if (choose_nat < 0.8) {
-                    list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(),drand48()*drand48())));
+                    list[i++] = new moving_sphere(center, center+vec3(0, 0.5*drand48(), 0),0.0, 1.0, 0.2, new lambertian(vec3(drand48()*drand48(), drand48()*drand48(),drand48()*drand48())));
                 } else if (choose_nat < 0.95) {
                     list[i++] = new sphere(center, 0.2, new metal(vec3(0.5*(1 + drand48()), 0.5*(1 + drand48()), 0.5*(1 + drand48())), 0.5*drand48()));
                 } else {
@@ -54,8 +54,8 @@ hitable *random_scene() {
 }
 
 int main() {
-    int nx = 400;
-    int ny = 200;
+    int nx = 200;
+    int ny = 100;
     int ns = 100;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     // hitable *list[5];
@@ -65,11 +65,11 @@ int main() {
     // list[3] = new sphere(vec3(-1,0,-1), 0.5, new dielectric(1.5));
     // list[4] = new sphere(vec3(-1,0,-1), -0.45, new dielectric(1.5));
     hitable *world = random_scene();
-    vec3 lookfrom(4,1,5);
-    vec3 lookat(0,0,-1);
-    float dist_to_focus = (lookfrom-lookat).length();
-    float apperture = 2.0;
-    camera cam(lookfrom, lookat, vec3(0,1,0), 50, float(nx)/float(ny), apperture, dist_to_focus);
+    vec3 lookfrom(13,2,3);
+    vec3 lookat(0,0,0);
+    float dist_to_focus = 10.0;
+    float apperture = 0.0;
+    camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), apperture, dist_to_focus, 0.0, 1.0);
     for(int j = ny-1; j >= 0; j--) {
         for(int i = 0; i < nx; i++) {
             vec3 col(0,0,0);
--- src/camera.h
+++ src/camera.h
@@ -12,7 +12,11 @@ vec3 random_in_unit_disc() {
 
 class camera {
     public: 
-        camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect, float apperture, float focus_dist) {
+        camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect, float apperture, float focus_dist,
+            float t0, float t1
+        ) {
+            time0 = t0;
+            time1 = t1;
             lens_radius = apperture / 2;
             float theta = vfov * M_PI/180;
             float half_height = tan(theta/2);
@@ -25,12 +29,22 @@ class camera {
             horizontal = 2*half_weight*focus_dist*u;
             vertical = 2*half_height*focus_dist*v;
         } 
-        ray get_ray(float u, float v) { return ray(origin, lower_left_corner + u*horizontal + v*vertical - origin);}
+        ray get_ray(float s, float t) { 
+            vec3 rd = lens_radius*random_in_unit_disc();
+            vec3 offset = u * rd.x() + v * rd.y();
+            float time = time0 + drand48() * (time1-time0);
+            return ray(
+                origin+offset,
+                lower_left_corner + s*horizontal + t*vertical - origin-offset,
+                time
+            );
+        }
 
         vec3 origin;
         vec3 lower_left_corner;
         vec3 horizontal;
         vec3 vertical;
         vec3 u, v, w;
+        float time1, time0;
         float lens_radius;
 };
